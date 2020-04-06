593dd23943fca8b25917e69c770d9bc4ae954ee2
Emmanuel Yirenkyi <fb_emmanuel@outlook.com>
2020-04-06 21:13:45 +0000

    Adding client modules and navbar to project

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { IProduct } from './models/products';
+import { IPagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  title = 'Skinnet';
+  products: IProduct[];
+
+
+  constructor(private http: HttpClient) {
+
+  }
+  ngOnInit(): void {
+    this.http.get('https://localhost:5002/api/products?pagesize=50').subscribe((response: IPagination) => {
+      this.products = response.data;
+      console.log(response.data);
+    }, error => {
+      console.log(error);
+    })
+  }
+
 }
